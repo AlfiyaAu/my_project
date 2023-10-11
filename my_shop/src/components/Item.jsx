709af459af7b8dbc661f1fbc209f3047ca120b7a
AlfiyaAu <147634126+AlfiyaAu@users.noreply.github.com>
2023-10-11 20:55:48 +0400

    Added images and change App.js and change Item.jsx
    Добавлена папка image  в public, добавлены картинки в app и выведены через item

--- my_shop/src/components/Item.jsx
+++ my_shop/src/components/Item.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Item (props){
     return(
         <div className="item">
-            <img src={""} />
+            <img src={"./images/"+ props.item2.img} alt="alt-txt"/>
             <h2> {props.item2.title}</h2>
             <p>{props.item2.desk}</p>
             <b>{props.item2.price}</b>
