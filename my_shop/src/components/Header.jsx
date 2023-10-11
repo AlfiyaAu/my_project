3b307fa67f7ba8bbe3e2c7b04be07292136ad45a
AlfiyaAu <147634126+AlfiyaAu@users.noreply.github.com>
2023-10-11 20:01:38 +0400

    Add base components and styles
    Добавлены Header, Footer, Item, Items, добавлены некоторые стили для этих компонентов.

---
+++ my_shop/src/components/Header.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default function Header (){
+    return(
+        <header>
+            <div>
+                <span className="logo"> My moda </span>
+            </div>
+
+            <ul className="nav">
+                <li> Про нас </li>
+                <li> Контакты </li>
+                <li> Личный кабинет </li>   
+            </ul>
+
+            <div className="presentation">
+            
+            </div>
+        </header>
+    );
+}
